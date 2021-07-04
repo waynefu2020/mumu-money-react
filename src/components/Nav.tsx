2221affbf9908982c5fca8e41384f2389bddcd74
DDWayne <652507565@qq.com>
2021-07-04 23:09:14 +0800

    引入导航栏icon，增加样式

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import React from 'react';
+require('icons/money.svg');
+require('icons/tag.svg');
+require('icons/statistics.svg')
+
 
 const NavWrapper = styled.nav`
   line-height: 24px;
@@ -8,9 +12,17 @@ const NavWrapper = styled.nav`
   > ul {
     display:flex;
     > li{
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
       width: 33.3333%;
+      padding: 4px 0;
       text-align:center;
-      padding: 16px;
+      > .icon{
+        width: 22px;
+        height: 22px;
+      }
     }
   }
 `
@@ -21,12 +33,21 @@ const Nav = ()=>{
         <NavWrapper>
             <ul>
                 <li>
+                    <svg className="icon">
+                        <use xlinkHref="#tag"/>
+                    </svg>
                     <Link to="/tags">标签页</Link>
                 </li>
                 <li>
+                    <svg className="icon">
+                        <use xlinkHref="#money"/>
+                    </svg>
                     <Link to="/money">记账页</Link>
                 </li>
                 <li>
+                    <svg className="icon">
+                        <use xlinkHref="#statistics"/>
+                    </svg>
                     <Link to="/statistics">统计页</Link>
                 </li>
             </ul>
