914152f9d7015dbb818ef643918bba11ce64cc1b
DDWayne <652507565@qq.com>
2021-07-05 15:08:53 +0800

    可以选中和新增标签

--- src/views/Money/TagsSection.tsx
+++ src/views/Money/TagsSection.tsx
@@ -1,38 +1,73 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
 
-const TagsSection:React.FC = ()=>{
-    return (
-        <_TagsSection>
-            <ol>
-                <li>衣服</li>
-                <li>食物</li>
-                <li>住房</li>
-                <li>交通</li>
-            </ol>
-            <button>新增标签</button>
-        </_TagsSection>
-    )
-}
+const TagsSection: React.FC = () => {
+    const [tags, setTags] = useState<string[]>(['衣服', '食物', '住房', '交通']);
+    const [selectedTags, setSelectedTags] = useState<string[]>([]);
+    const onAddTag = () => {
+        const tagName = window.prompt('请输入标签名');
+        if (tagName !== null) {
+            setTags([...tags, tagName]);
+        }
+    };
+    const onToggleTag = (tag: string) => {
+        const index = selectedTags.indexOf(tag);
+        if (index >= 0) {
+            setSelectedTags(selectedTags.filter(t => t !== tag));
+        } else {
+            setSelectedTags([...selectedTags, tag]);
+        }
 
-const _TagsSection = styled.section`
-  background: #FFFFFF; padding: 12px 16px;
+    };
+    const getClass = (tag: string) => selectedTags.indexOf(tag) >= 0 ? 'selected' : '';
+    return (
+        <Wrapper>
+            <ol>
+                {tags.map(tag =>
+                    <li key={tag} onClick={() => {onToggleTag(tag);}}
+                        className={getClass(tag)}
+                    >{tag}</li>
+                )}
+            </ol>
+            <button onClick={onAddTag}>新增标签</button>
+        </Wrapper>
+    );
+};
+
+const Wrapper = styled.section`
+  background: #FFFFFF;
+  padding: 12px 16px;
   flex-grow: 1;
   display: flex;
   flex-direction: column;
   justify-content: flex-end;
   align-items: flex-start;
-  > ol { margin: 0 -12px;
-    > li{
-      background: #D9D9D9; border-radius: 18px;
-      display:inline-block; padding: 3px 18px;
-      font-size: 14px; margin: 8px 12px;
+
+  > ol {
+    margin: 0 -12px;
+
+    > li {
+      background: #D9D9D9;
+      border-radius: 18px;
+      display: inline-block;
+      padding: 3px 18px;
+      font-size: 14px;
+      margin: 8px 12px;
+
+      &.selected {
+        background: #ef8871;
+        color: white;
+      }
     }
   }
-  > button{
-    background:none; border: none; padding: 2px 4px;
-    border-bottom: 1px solid #333; color: #666;
+
+  > button {
+    background: none;
+    border: none;
+    padding: 2px 4px;
+    border-bottom: 1px solid #333;
+    color: #666;
     margin-top: 8px;
   }
-`
-export {TagsSection}
+`;
+export {TagsSection};
