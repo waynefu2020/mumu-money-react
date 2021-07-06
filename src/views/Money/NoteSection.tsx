e84119137895effe8a4ec6100f75b55e1c02a047
DDWayne <652507565@qq.com>
2021-07-06 17:15:51 +0800

    重构备注功能

--- src/views/Money/NoteSection.tsx
+++ src/views/Money/NoteSection.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
-import React, {useRef} from 'react';
+import React, {ChangeEventHandler, useRef} from 'react';
+import {Input} from '../../components/Input';
 
 type Props = {
     value: string,
@@ -9,20 +10,15 @@ type Props = {
 const NoteSection: React.FC<Props> = (props) => {
     const note = props.value
     const refInput = useRef<HTMLInputElement>(null);
-    const onBlur = () => {
-        if (refInput.current !== null) {
-            props.onChange(refInput.current.value);
-        }
+    const onChange:ChangeEventHandler<HTMLInputElement> = (e) => {
+        props.onChange(e.target.value)
     };
     console.log(note);
     return (
         <Wrapper>
-            <label>
-                <span>备注</span>
-                <input type="text" placeholder="点击填写备注"
-                       defaultValue={note} onBlur={onBlur} ref={refInput}
-                />
-            </label>
+            <Input label="备注" type="text"
+                   value={note} onChange={onChange}
+            />
         </Wrapper>
     );
 };
@@ -31,23 +27,6 @@ const Wrapper = styled.section`
   background: #f5f5f5;
   padding: 0 16px;
   font-size: 14px;
-
-  > label {
-    display: flex;
-    align-items: center;
-
-    > span {
-      margin-right: 16px;
-      white-space: nowrap;
-    }
-
-    > input {
-      display: block;
-      width: 100%;
-      height: 72px;
-      background: none;
-      border: none;
-    }
-  }
+  
 `;
 export {NoteSection};
