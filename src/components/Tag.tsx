1d9247b95279f70cefbae721af252056890a3001
DDWayne <652507565@qq.com>
2021-07-06 18:12:15 +0800

    可以删除tag

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -29,32 +29,39 @@ const InputWrapper = styled.div`
 `;
 
 const Tag: React.FC = (props) => {
-    const {findTag, updateTag} = useTags();
+    const {findTag, updateTag, deleteTag} = useTags();
     let {id: idString} = useParams<Params>();
     const tag = findTag(parseInt(idString));
-    return (
-        <Layout>
-            <TopBar>
-                <Icon name="left"/>
-                <span>编辑标签</span>
-                <Icon/>
-            </TopBar>
-            <InputWrapper>
-                <Input label="标签名" type="text"
-                       placeholder="填写标签名" value={tag.name}
-                       onChange={(e) => {
-                           updateTag(tag.id, {name: e.target.value});
-                       }}
-                />
-            </InputWrapper>
-            <Center>
-                <Space/>
-                <Space/>
-                <Space/>
-                <Button>删除标签</Button>
-            </Center>
-        </Layout>
-    );
+    if(tag){
+        return (
+            <Layout>
+                <TopBar>
+                    <Icon name="left"/>
+                    <span>编辑标签</span>
+                    <Icon/>
+                </TopBar>
+                <InputWrapper>
+                    <Input label="标签名" type="text"
+                           placeholder="填写标签名" value={tag.name}
+                           onChange={(e) => {
+                               updateTag(tag.id, {name: e.target.value});
+                           }}
+                    />
+                </InputWrapper>
+                <Center>
+                    <Space/>
+                    <Space/>
+                    <Space/>
+                    <Button onClick={()=>deleteTag(tag.id)}>删除标签</Button>
+                </Center>
+            </Layout>
+        );
+    } else {
+        return (
+            <div>tag已删除</div>
+        )
+    }
+
 };
 
 export {Tag};
