c146906e897a6c4bab48ead396904cc788ef263c
DDWayne <652507565@qq.com>
2021-07-12 15:24:21 +0800

    管理标签样式1

--- src/views/Money/Setting.tsx
+++ src/views/Money/Setting.tsx
@@ -3,6 +3,7 @@ import {CategorySection} from './CategorySection';
 import {useTags} from '../../hooks/useTags';
 import {CategoryWrapper, defaultFormData} from '../Money';
 import Icon from '../../components/Icon';
+import styled from 'styled-components';
 
 
 const Setting:React.FC= (props) =>{
@@ -21,17 +22,33 @@ const Setting:React.FC= (props) =>{
                                  onChange={(category) => onChange({category})}
                 />
             </CategoryWrapper>
-            <ul>
+            <ItemWrapper>
+                <span>创建新的标签</span>
                 {iconTags.map(t=>
                     <li key={t.id} className={getClass(t.id)}>
                         <Icon name={t.svg}/>
                         {t.name}
+                        <Icon name="delete"/>
                     </li>
                 )}
-            </ul>
+            </ItemWrapper>
         </div>
 
     )
 }
+
+const ItemWrapper = styled.ol`
+  display: flex;
+  flex-direction: column;
+   > li{
+     border-bottom: 1px solid black;
+     padding: 10px 10px;
+     > svg{
+       margin: 4px 0;
+       width: 20px;
+       height: 20px;
+     }
+   }
+`
 
 export {Setting}
